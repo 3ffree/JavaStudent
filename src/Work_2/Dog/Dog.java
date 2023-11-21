98e164c721e0c48e4057b5369aa49968dfa057f8
3ffree <impersona1ityu@gmail.com>
2023-11-21 16:02:58 +0300

    Test commit for push testing purposes -Added Dog and DgoShelter classes

---
+++ src/Work_2/Dog/Dog.java
@@ -0,0 +1,39 @@
+package Work_2.Dog;
+
+public class Dog {
+    private int age;
+    private String name;
+
+    public Dog(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int ageToHuman() {
+        return this.age * 7;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/Work_2/Dog/DogShelter.java
@@ -0,0 +1,11 @@
+package Work_2.Dog;
+
+public class DogShelter {
+    Dog[] list;
+    int size;
+
+    public DogShelter(int size) {
+        this.list = new Dog[size];
+        this.size = size;
+    }
+}
